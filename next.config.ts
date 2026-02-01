04f33d8c40a653a33c4178532b0d0fe8152eb7fd
modernscape <modernscape@gmail.com>
2026-02-02 07:15:16 +0900

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // 静的書き出しを有効化
+  images: {
+    unoptimized: true, // GitHub Pagesでは画像の最適化が使えないため必須
+  },
+  // リポジトリ名が 'my-app' の場合、'/my-app' と指定が必要（カスタムドメインを使わない場合）
+  // basePath: '/repo-name',
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default nextConfig
